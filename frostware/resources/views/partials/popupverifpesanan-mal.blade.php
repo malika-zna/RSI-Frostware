a8a5e071520d1da836d18a4ccfdecc2bf901907b
malika <malika.zna.27@gmail.com>
2025-10-29 18:53:57 +0700

    ini bingung detail pesanan sama terima pesanannya gimana

--- frostware/resources/views/partials/popupverifpesanan-mal.blade.php
+++ frostware/resources/views/partials/popupverifpesanan-mal.blade.php
@@ -76,7 +76,7 @@
                         <button class="btn btn-tolak">
                             <div class="text">Tolak</div>
                         </button>
-                        <button class="btn btn-terima">
+                        <button class="btn btn-terima"></button>
                             <div class="text">Terima</div>
                         </button>
                     </div>
