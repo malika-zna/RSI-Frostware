4ac606bf705f440f9604bca8eba9c1cc7304635b
Chann <cantikazahna@student.ub.ac.id>
2025-11-02 11:51:37 +0700

    ubah view kelola aset

--- frostware/resources/views/KelolaAset-can/dashboard.blade.php
+++ frostware/resources/views/KelolaAset-can/dashboard.blade.php
@@ -2,20 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Parkinsans:wght@300..800&display=swap"
+        rel="stylesheet">
     <title>Dashboard Pemeliharaan - Frostware</title>
     <style>
         /* CSS layout*/
-        body {
+        /* body {
             margin: 0;
             font-family: 'Inter', sans-serif;
             background: #FFFFFF;
             width: 1280px;
             height: 832px;
             position: relative;
+        } */
+        
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: Inter, sans-serif;
+        }
+        body {
+            width: 100vw;
+            height: 100vh;
+            overflow-x: hidden;
+            background: #FFFFFF;
         }
 
         /* Header */
-        header {
+        /* header {
             position: absolute;
             width: 1280px;
             height: 90px;
@@ -46,6 +64,160 @@
             font-size: 13px;
             font-weight: 400;
             margin-top: 3px;
+        } */
+
+        header { /* bg header */
+            background-color: #1C398E;
+            box-shadow: -5px 5px 8px rgba(0, 0, 0, 0.25);
+            width: 100vw;
+            height: 90px;
+            box-sizing: border-box;
+            top: 0px;
+            left: 0px;
+            position: fixed;
+            padding: 20px 40px;
+            display: flex;
+            flex-direction: row;
+            z-index: 1;
+
+            .head-title {
+                color: white;
+                font-size: 26px;
+                font-weight: 600;
+                margin-right: auto;
+                font-family: Parkinsans;
+            }
+
+            .head-info {
+                display: inline-flex;
+                flex-direction: column;
+                align-items: flex-end;
+                gap: 5px;
+            }
+
+            .user-info {
+                border: none;
+                background: none;
+                display: inline-flex;
+                flex-direction: row;
+                gap: 5px;
+
+                .user {
+                    color: white;
+                    font-size: 16px;
+                    font-weight: 500;
+                }
+
+                svg {
+                    width: 10px;
+                }
+            }
+
+            .user-info:hover {
+                cursor: pointer;
+                background-color: rgba(255, 255, 255, 0.06);
+            }
+
+
+            .date {
+                padding-right: 8px;
+                display: inline-flex;
+                flex-direction: row;
+                gap: 5px;
+
+                p {
+                    color: rgba(255, 255, 255, 0.60);
+                    font-size: 12px;
+                    font-weight: 400;
+                }
+            }
+
+        }
+
+        .user-panel {
+            width: 280px;
+            height: fit-content;
+            padding: 20px 25px 15px 25px;
+            background: #000;
+            /* display: inline-flex; */
+            display: none;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: flex-start;
+            gap: 5px;
+            box-sizing: border-box;
+            position: fixed;
+            right: 20px;
+            top: 50px;
+            z-index: 100;
+
+            .user-role {
+                color: #fff;
+                font-size: 16px;
+                font-family: Parkinsans;
+                /* font-family: Inter, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial; */
+                font-weight: 600;
+                margin-bottom: 5px;
+                /* line-height: 16px; */
+                /* word-wrap: break-word; */
+            }
+
+            .user-email {
+                color: rgba(255, 255, 255, 0.60);
+                font-size: 12px;
+                /* font-family: Inter, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial; */
+                font-weight: 400;
+                /* line-height: 16px; */
+                /* word-wrap: break-word; */
+            }
+
+            .user-name {
+                color: #fff;
+                font-size: 14px;
+                /* font-family: Inter, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial; */
+                font-weight: 400;
+                /* line-height: 16px; */
+                /* word-wrap: break-word; */
+            }
+
+            .user-divider {
+                align-self: stretch;
+                height: 5px;
+                /* position: relative; */
+                border-bottom: 1px solid rgba(255, 255, 255, 0.60);
+                box-sizing: border-box;
+                margin-top: 5px;
+                margin-bottom: 5px;
+            }
+
+            form {
+                margin-left: auto;
+            }
+
+            .user-actions {
+                /* overflow: hidden; */
+                display: inline-flex;
+                justify-content: flex-end;
+                align-items: center;
+                gap: 10px;
+                margin-left: auto;
+                padding: 5px 10px;
+                border-radius: 3px;
+            }
+
+            button.user-actions:hover {
+                cursor: pointer;
+                background-color: rgba(255, 255, 255, 0.2);
+            }
+
+            .logout-text {
+                color: #000;
+                font-size: 14px;
+                /* font-family: Inter, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial; */
+                font-weight: 400;
+                /* line-height: 16px; */
+                /* word-wrap: break-word; */
+            }
         }
 
         /* body konten*/
@@ -53,10 +225,19 @@
             position: absolute;
             top: 111px; /* 90px + 21px margin */
             left: 40px;
-            width: 1200px;
-            height: 700px;
+            /* width: 1200px; */
+            width: 100%; /* ubah width */
+            max-width: 1280px; /* batas width maks */ 
+            /* height: 700px; */
             display: flex;
             justify-content: space-between;
+        }
+
+        /* Bagian kiri dan kanan */
+        .left, .right {
+            /* width: 540px; */
+            width: 48%; /* ubah width */
+            position: relative;
         }
 
         /* Button Style */
@@ -78,13 +259,7 @@
             font-family: 'Inter', sans-serif;
             cursor: pointer;
         }
-
-        /* Bagian kiri dan kanan */
-        .left, .right {
-            width: 540px;
-            position: relative;
-        }
-
+        
         /* Title Button Positioning */
         .left .btn-main {
             position: absolute;
@@ -98,7 +273,7 @@
         }
 
         /* Garis pembatas vertikal */
-        .container::after {
+        /* .container::after {
             content: '';
             display: block;
             position: absolute;
@@ -107,7 +282,7 @@
             width: 1px;
             height: 700px;
             background: rgba(0, 0, 0, 0.1);
-        }
+        } */
 
         /* Card umum */
         .card {
@@ -216,13 +391,32 @@
 </head>
 <body>
 
+<!-- html -->
 <header>
-    <h1>Frostware</h1>
-    <div class="profile">
-        Penanggung Jawab Pemeliharaan
-        <small>Minggu, 28/09/2025</small>
+    <h1 class="head-title">Frostware</h1>
+    <div class="head-info">
+        <div class="user-info" onclick="togglePanel()">
+            <span class="user">Penanggung Jawab Pemeliharaan</span>
+        </div>
+        <div class="date">
+            <p>Minggu, 28/09/2025</p>
+        </div>
     </div>
 </header>
+
+<!-- tambahan user profil -->
+<div class="user-panel" id="userPanel">
+        <div class="user-role">{{ session('role', 'Role') }}</div>
+        <div class="user-name">{{ session('nama', 'nama pengguna') }}</div>
+        <div class="user-email">{{ session('email', 'email') }}</div>
+        <div class="user-divider"></div>
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button class="user-actions" type="submit" logout>
+                <div class="logout-text">Logout</div>
+            </button>
+        </form>
+</div>
 
 <div class="container">
 
@@ -314,6 +508,14 @@
     </section>
 
 </div>
+
+<!-- tambahan js -->
+<script>
+function togglePanel() {
+    const panel = document.getElementById('userPanel');
+    panel.style.display = panel.style.display === 'flex' ? 'none' : 'flex';
+    }
+</script>
 
 </body>
 </html>
