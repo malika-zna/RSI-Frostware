d9811f17145187a42825fdd9eac1cf4cf0a7a20d
Chann <cantikazahna@student.ub.ac.id>
2025-11-17 14:15:46 +0700

    ubah nama migration lagiii

--- frostware/database/migrations/2025_10_30_092920_create_asets_table.php
+++ frostware/database/migrations/2025_10_30_092920_create_asets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class create_asets_table extends Migration
+class CreateAsetsTable extends Migration
 {
     public function up()
     {
